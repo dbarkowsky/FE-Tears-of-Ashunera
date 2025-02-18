60ade658e7c9fa9ab5554432b40748c3556f84f2
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-02-18 10:41:24 -0800

    update config and css

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      
     },
   }),
 }
@@ -31,7 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     // Component.Graph(),
-    // Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.TableOfContents()),
     // Component.Backlinks(),
   ],
 }
